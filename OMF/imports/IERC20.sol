013866fa16fc2c8970392d30c9897d78a849a968
Elix <119566415+GenericMage@users.noreply.github.com>
2025-04-09 21:44:14 +0100

    Add files via upload

--- OMF/imports/IERC20.sol
+++ OMF/imports/IERC20.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.1;
 
 interface IERC20 {
     function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256); // Added for OMFAgent prepListing
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
 }
